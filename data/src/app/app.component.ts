49c0c944c6621123ffe3833198b3535fa6632ba3
yayanheeh <tokoxp58@gmail.com>
2018-02-26 20:13:42 +0700

    Add files via upload

---
+++ data/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AdService }         from './ad.service';
+import { AdItem }            from './ad-item';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      <app-add-banner [ads]="ads"></app-add-banner>
+    </div>
+  `
+})
+export class AppComponent implements OnInit {
+  ads: AdItem[];
+
+  constructor(private adService: AdService) {}
+
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+  }
+}
+
